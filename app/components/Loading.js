5c66083ba92d306336118fb2ddedc7eeb9ce0d97
Juan E. Chemello <juanchemell@gmail.com>
2021-10-03 00:53:27 -0300

    Added loader in account tab

---
+++ app/components/Loading.js
@@ -0,0 +1,27 @@
+import React from "react"
+import { StyleSheet, View, Text, ActivityIndicator } from "react-native"
+
+export default function Loading(props) {
+  const { text } = props
+
+  return (
+    <View style={styles.view}>
+      <ActivityIndicator size="large" color="#00a680" />
+      {text && <Text style={styles.text}>{text}</Text>}
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  view: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "rgba(0,0,0,0.25)"
+  },
+  text: {
+    color: "#00a680",
+    textTransform: "uppercase",
+    marginTop: 10
+  }
+})
